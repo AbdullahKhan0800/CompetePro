e4b8a4899ffc14ac9ca6fea85f1a8ea384be08fb
Abdullah Khan <abdullah.khan@ecomexperts.io>
2025-01-24 04:32:20 +0500

    Final V2 Uplaoded

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import './Components/Assets/Global.css'
-import Menu from './Components/Menu';
-import Particle from './Components/Particle';
-import Contact from './Components/Pages/Contact'
-import About from './Components/Pages/About'
-import Services from './Components/Pages/Services'
-import Hero from './Components/Hero'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-function App() {
-  return (
-    <>
-      <Router>
-        <Particle />
-        <Menu />
-        <Routes>
-          <Route path='/' element={<Hero />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/services' element={<Services />} />
-        </Routes>
-      </Router>
-    </>
-  );
-}
-
-export default App;
-=======
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from "./Layout";
@@ -51,4 +22,3 @@ function App() {
 }
 
 export default App;
->>>>>>> v.1.1/main
